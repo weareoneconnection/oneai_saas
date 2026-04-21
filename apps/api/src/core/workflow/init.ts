4561eb706aab92f3b0d5c789e6f8058536922f1b
weareoneconnection <506473839open@gmail.com>
2026-04-22 00:33:03 +0800

    update: latest changes

--- apps/api/src/core/workflow/init.ts
+++ apps/api/src/core/workflow/init.ts
@@ -42,6 +42,4 @@ import "./onefield_intelligence_workflow.js";
 import "./oneMirrorWorkflow.js";
 
 
-import "./marketIntelligenceWorkflow.js";
-import "./marketAnalysisWorkflow.js";
-import "./marketDecisionWorkflow.js";
+
